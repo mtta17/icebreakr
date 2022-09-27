0462746a0d2062cb314a78b17ad8ed3e0809e958
Matthew Simmons <mtta17@gmail.com>
2022-09-26 21:28:42 -0400

    adding a legacy .r file to test

---
+++ R/html_tables.r
@@ -0,0 +1,29 @@
+# html_tables.r
+#
+# **Description ---------------------------------------------------------------
+# A clean wrapper(s)(?) for creating a friendly HTML table in a markdown doc.
+# author - mtta17@gmail.com
+#
+# html_tables
+#
+# fancyTable ############################################################
+# **Arguments -----------------------------------------------------------------
+# df		data frame that you want as a table
+# title		string to give a title to your table
+# **Output --------------------------------------------------------------------
+# A nice, formatted HTML table to insert into a document
+# **Dependencies --------------------------------------------------------------
+# DT, htmltools
+
+fancyTable <- function(df, title = quote()){
+    df %>%
+        DT::datatable(class = 'display compact nowrap',
+                      caption = htmltools::tags$caption(style = 'caption-side: top; text-align: left;', htmltools::h3(title)),
+                      filter = "top",
+                      extensions = 'Scroller',
+                      options = list(
+                          deferRender = TRUE,
+                          scrollX = T,
+                          scrollY = 400,
+                          scroller = TRUE))
+}
